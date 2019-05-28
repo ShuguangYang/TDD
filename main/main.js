17d6a27bca9df751073aa6b1a0c8d0ce77cfee75
ShuguangYang <437012461@qq.com>
2019-05-28 22:35:26 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price=0;
+    if(inputs.distance<=2){
+    	price=price+6;
+    }
+    else if(inputs.distance>2&&inputs.distance<=8){
+    	price=6+(inputs.distance-2)*0.8;
+    }
+    else{
+    	price=6+6*0.8+(inputs.distance-8)*1.2;
+    }
+    price = price + inputs.parkTime * 0.25;
+    price=Math.round(price);
+    return price;
 };
